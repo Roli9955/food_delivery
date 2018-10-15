90de65532c2eade3c7ed3990f24b65c104084f43
Kotroczó Roland <k.roli9955@gmail.com>
2018-10-15 22:43:18 +0200

    Projekt létrehozása

---
+++ BackEnd/FoodDelivery/src/main/java/hu/elte/Food_delivery/FoodDeliveryApplication.java
@@ -0,0 +1,12 @@
+package hu.elte.Food_delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodDeliveryApplication.class, args);
+	}
+}
